11677f452967b6623c49cb5568eb36e8e68e6591
ayb1337 <aero.glowsky@gmail.com>
2023-06-17 02:05:06 +0100

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,21 +13,26 @@
 
 int main(void)
 {
-int i, j;
+int i, j, k;
 i = 0;
-
 
 while (i < 10)
 {
 j = i + 1;
 while (j < 10)
 {
+k = j + 1;
+while (k < 10)
+{
 putchar(i + '0');
 putchar(j + '0');
-if (i != 8 || j != 9)
+putchar(k + '0');
+if (i != 7 || j != 8 || k != 9)
 {
 putchar(',');
 putchar(' ');
+}
+k++;
 }
 j++;
 }
