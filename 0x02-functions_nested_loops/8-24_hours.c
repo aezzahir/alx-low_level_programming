1288e99a28343b615b486d060e635ab868950c4d
ayb1337 <aero.glowsky@gmail.com>
2023-06-21 00:55:58 +0100

    8-24_hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,4 +1,5 @@
 #include "main.h"
+void to_digit(int n);
 
 /**
  * jack_bauer - Write a function that prints the last digit of a number.
@@ -8,53 +9,30 @@
 
 void jack_bauer(void)
 {
-int i, j, r1, r2;
+int i, j;
 for (i = 0; i < 24; i++)
 {
 for (j = 0; j < 60; j++)
 {
-if (i < 10)
-{
-_putchar('0');
-_putchar('0' + i);
+to_digit(i);
 _putchar(':');
-if (j < 10)
-{
-_putchar('0');
-_putchar('0' + j);
+to_digit(j);
 _putchar('\n');
 }
-else
+}
+}
+/**
+ * to_digit - function
+ * @n: The number.
+ * Retur : digits of number
+*/
+
+void to_digit(int n)
 {
-r1 = j % 10;
-r2 = (j - r1) / 10;
+int r1, r2;
+r1 = n % 10;
+r2 = (n - r1) / 10;
 _putchar('0' + r2);
 _putchar('0' + r1);
-_putchar('\n');
 }
-}
-else
-{
-r1 = i % 10;
-r2 = (i - r1) / 10;
-_putchar('0' + r2);
-_putchar('0' + r1);
-_putchar(':');
-if (j < 10)
-{
-_putchar('0');
-_putchar('0' + j);
-_putchar('\n');
-}
-else
-{
-r1 = j % 10;
-r2 = (j - r1) / 10;
-_putchar('0' + r2);
-_putchar('0' + r1);
-_putchar('\n');
-}
-}
-}
-}
-}
+
