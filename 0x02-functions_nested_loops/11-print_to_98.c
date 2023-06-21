fc659ce8a9668e0cde90cab540d4ec28ddd02f18
ayb1337 <aero.glowsky@gmail.com>
2023-06-21 10:54:51 +0100

    last one

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,29 +13,30 @@ void print_to_98(int n)
 {
 int i;
 if (n > 98)
-    {
-        for (i = n; i >= 98; i--)
-        {
-            print_digits(i);
-            if (i != 98)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-    }
-    else
-    {
-        for (i = n; i <= 98; i++)
-        {
-            print_digits(i);
-            if (i != 98)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-    }
+{
+for (i = n; i >= 98; i--)
+{
+print_digits(i);
+if (i != 98)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+}
+else
+{
+for (i = n; i <= 98; i++)
+{
+print_digits(i);
+if (i != 98)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+}
+_putchar('\n');
 }
 
 /**
@@ -46,14 +47,14 @@ if (n > 98)
 
 void print_digits(int n)
 {
-    if(n < 0)
-    {
-        _putchar('-');
-        n = -n;
-    }
+if (n < 0)
+{
+_putchar('-');
+n = -n;
+}
 
-    if (n / 10)
-        print_digits(n / 10);
+if (n / 10)
+print_digits(n / 10);
 
-    _putchar('0' + n % 10);
+_putchar('0' + n % 10);
 }
