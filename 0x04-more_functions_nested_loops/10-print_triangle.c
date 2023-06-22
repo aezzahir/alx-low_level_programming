7f823f17fb6cc15793b5c18ff4fd49d1949bcb97
ayb1337 <aero.glowsky@gmail.com>
2023-06-22 16:20:59 +0100

    10. Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,23 +13,23 @@ int i, j, k, limit;
 k = 0;
 for (i = 1; i <= size; i++)
 {
-    limit = size - k;
-    for (j = 1; j <= size; j++)
-    {
-        if (j < limit)
-        {
-            _putchar(' ');
-        }
-        else
-        {
-            _putchar('#');
-        }
-    }
-    _putchar('\n');
-    k = k + 1;
+limit = size - k;
+for (j = 1; j <= size; j++)
+{
+if (j < limit)
+{
+_putchar(' ');
+}
+else
+{
+_putchar('#');
+}
+}
+_putchar('\n');
+k = k + 1;
 }
 if (size <= 0)
 {
-    _putchar('\n');
+_putchar('\n');
 }
 }
