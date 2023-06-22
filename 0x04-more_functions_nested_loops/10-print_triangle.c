b274beeda6c81d24654afcb09587c40158f574bc
ayb1337 <aero.glowsky@gmail.com>
2023-06-22 16:19:29 +0100

    10. Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_triangle - draws a triangle.
+ * @size : size of the triangle
+ * Return: void.
+ */
+
+void print_triangle(int size)
+{
+int i, j, k, limit;
+k = 0;
+for (i = 1; i <= size; i++)
+{
+    limit = size - k;
+    for (j = 1; j <= size; j++)
+    {
+        if (j < limit)
+        {
+            _putchar(' ');
+        }
+        else
+        {
+            _putchar('#');
+        }
+    }
+    _putchar('\n');
+    k = k + 1;
+}
+if (size <= 0)
+{
+    _putchar('\n');
+}
+}
