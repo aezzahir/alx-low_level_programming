dab48dea36fc820fb5e18cd8fed4840f8ffc73e6
Ayoub EZZAHIRI <aero.glowsky@gmail.com>
2023-07-01 01:31:19 -0700

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+
+/**
+ * *_strcat - concatenates two strings.
+ * @dest: dest string
+ * @src: src string
+ * Return: a pointer to the resulting string dest
+ */
+
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+
+	j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != '\0')
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+
+	return (dest);
+}
+
