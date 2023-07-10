c2657645bdc73a19a4984adbe1f593b2d8f9c255
ayb1337 <aero.glowsky@gmail.com>
2023-07-11 00:03:48 +0100

    101

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,67 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * count_words - Counts the number of words in a string
+ * @str: The string to count
+ *
+ * Return: The number of words in the string
+ */
+int count_words(char *str)
+{
+    int i, count = 0;
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
+            count++;
+    }
+
+    return (count);
+}
+
+/**
+ * strtow - Splits a string into words
+ * @str: The string to split
+ *
+ * Return: Pointer to the array of words
+ */
+char **strtow(char *str)
+{
+    char **words;
+    int i, j, k, m, words_count, len;
+
+    if (str == NULL || str[0] == '\0')
+        return (NULL);
+
+    words_count = count_words(str);
+    if (words_count == 0)
+        return (NULL);
+
+    words = malloc((words_count + 1) * sizeof(char *));
+    if (words == NULL)
+        return (NULL);
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+        {
+            for (len = 0; str[i + len] != ' ' && str[i + len] != '\0'; len++);
+            words[j] = malloc((len + 1) * sizeof(char));
+            if (words[j] == NULL)
+            {
+                for (k = 0; k < j; k++)
+                    free(words[k]);
+                free(words);
+                return (NULL);
+            }
+            for (m = 0; m < len; m++)
+                words[j][m] = str[i + m];
+            words[j][m] = '\0';
+            j++;
+        }
+    }
+    words[j] = NULL;
+
+    return (words);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -7,5 +7,6 @@ int **alloc_grid(int width, int height);
 char *str_concat(char *s1, char *s2);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
+char **strtow(char *str);
 
 #endif /*MAIN_H*/
