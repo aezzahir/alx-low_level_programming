49a098641e0fda748d6bd170aa0dd455ac9201c4
Ayoub EZZAHIRI <aero.glowsky@gmail.com>
2023-07-10 16:38:44 -0700

    1001

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,15 +9,16 @@
  */
 int count_words(char *str)
 {
-    int i, count = 0;
+	int i, count = 0;
 
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
-            count++;
-    }
+	i = 0;
 
-    return (count);
+	for (; str[i] != '\0'; i++)
+	{
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
+			count++;
+	}
+	return (count);
 }
 
 /**
@@ -28,40 +29,40 @@ int count_words(char *str)
  */
 char **strtow(char *str)
 {
-    char **words;
-    int i, j, k, m, words_count, len;
+	char **words;
+	int i, j, k, m, words_count, len;
 
-    if (str == NULL || str[0] == '\0')
-        return (NULL);
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
+	words_count = count_words(str);
+	if (words_count == 0)
+		return (NULL);
 
-    words_count = count_words(str);
-    if (words_count == 0)
-        return (NULL);
-
-    words = malloc((words_count + 1) * sizeof(char *));
-    if (words == NULL)
-        return (NULL);
-
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
-        {
-            for (len = 0; str[i + len] != ' ' && str[i + len] != '\0'; len++);
-            words[j] = malloc((len + 1) * sizeof(char));
-            if (words[j] == NULL)
-            {
-                for (k = 0; k < j; k++)
-                    free(words[k]);
-                free(words);
-                return (NULL);
-            }
-            for (m = 0; m < len; m++)
-                words[j][m] = str[i + m];
-            words[j][m] = '\0';
-            j++;
-        }
-    }
-    words[j] = NULL;
-
-    return (words);
+	words = malloc((words_count + 1) * sizeof(char *));
+	if (words == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+		{
+			len = 0;
+			for (; str[i + len] != ' ' && str[i + len] != '\0'; len++);
+			words[j] = malloc((len + 1) * sizeof(char));
+			if (words[j] == NULL)
+			{
+				k = 0;
+				for (; k < j; k++)
+					free(words[k]);
+				free(words);
+				return (NULL);
+			}
+			for (m = 0; m < len; m++)
+				words[j][m] = str[i + m];
+			words[j][m] = '\0';
+			j++;
+		}
+	}
+	words[j] = NULL;
+	return (words);
 }
+
