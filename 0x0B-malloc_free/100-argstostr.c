773391860bd7d6fd7f27d6431eeb227b34f75fc2
ayb1337 <aero.glowsky@gmail.com>
2023-07-10 23:55:46 +0100

    100 é

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all arguments of your program
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: pointer to the new string
+ */
+char *argstostr(int ac, char **av)
+{
+int i, j, k, len;
+char *s;
+
+/* validate input */
+if (ac == 0 || av == NULL)
+{
+return (NULL);
+}
+/* first, calculate the length of the final string */
+i = 0;
+len = 0;
+for (; i < ac; i++)
+{
+j = 0;
+for (; av[i][j] != '\0'; j++, len++)
+{
+len++;
+}
+}
+len++;
+s = malloc(len *sizeof(char));
+if (s == NULL)
+{
+return (NULL);
+}
+for (i = 0, k = 0; i < ac; i++)
+{
+for (j = 0; av[i][j] != '\0'; j++, k++)
+{
+s[k] = av[i][j];
+}
+s[k] = '\n';
+k++;
+}
+s[k] = '\0';
+return (s);
+}
