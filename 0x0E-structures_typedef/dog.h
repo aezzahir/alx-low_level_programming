3f9e26fef65095252f4e91684d8e5b1786980198
ayb1337 <aero.glowsky@gmail.com>
2023-07-18 00:23:21 +0100

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _0_DOG_H_
+#define _0_DOG_H_
+
+/**
+ * struct dog - Dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+
+#endif /* _0_DOG_H_ */
