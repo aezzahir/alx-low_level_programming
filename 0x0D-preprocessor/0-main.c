4a5064adcf1c1ae7270af2d423760dcac5dd9c71
ayb1337 <aero.glowsky@gmail.com>
2023-07-17 13:51:55 +0100

    0. Object-like Macro

--- 0x0D-preprocessor/0-main.c
+++ 0x0D-preprocessor/0-main.c
@@ -1,7 +1,6 @@
 #include "0-object_like_macro.h"
 #include "0-object_like_macro.h"
 #include <stdio.h>
-#define SIZE 1024
 /**
  * main - check the code
  *
--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,4 +1,6 @@
-#ifdef MAIN_H
-#define MAIN_H
+#ifndef _0_OBJECT_LIKE_MACRO_H_
+#define _0_OBJECT_LIKE_MACRO_H_
+
 #define SIZE 1024
-#endif
+
+#endif /* _0_OBJECT_LIKE_MACRO_H_ */
