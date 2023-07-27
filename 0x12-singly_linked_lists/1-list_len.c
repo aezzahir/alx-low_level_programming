21f167d8b9cf9d704f3f2106cf1bff18836d0015
ayb1337 <aero.glowsky@gmail.com>
2023-07-27 14:51:48 +0100

    0. Print list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,12 +2,12 @@
 
 
 /**
- * print_list - prints all the elements of a list_t list
+ * list_len - prints all the elements of a list_t list
  * @h: the list_t list to print all the elements of
  * Return: returns the number of nodes
  */
 
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 int n = 0;
 const list_t *tmp;
