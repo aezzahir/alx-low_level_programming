3a391de0cd12235766cbfe192df359e8cbf20914
ayb1337 <aero.glowsky@gmail.com>
2023-07-28 01:43:13 +0100

    task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,50 +1,60 @@
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
+#include <string.h>
 
 /**
- * add_node_end - Adds a new node at the end of a list_t list.
- * @head: Pointer to a pointer that points to the head of the list.
- * @str: The string to be added to the new node.
- *
- * Return: The address of the new element, or NULL if it fails.
+ * _strlen - returns the length of a string
+ * @s: string to return the length of
+ * Return: returns the length of the string
+ */
+
+int strlen(const char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+return (i);
+}
+
+/**
+ * add_node_end - adds a new node at the end of a list_t list
+ * @head: pointer to pointer to the head node
+ * @str: value of string of node to be added
+ * Return: returns the address of the new element, or NULL if it failed
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    char *dup_str = strdup(str);
-    list_t *new_node = malloc(sizeof(list_t));
-
-    if (str == NULL)
-    {
-        return (NULL);
-    }
-    if (dup_str == NULL)
-    {
-        return (NULL);
-    }
-    if (new_node == NULL)
-    {
-        free(dup_str);
-        return (NULL);
-    }
-
-    new_node->str = dup_str;
-    new_node->len = strlen(str);
-    new_node->next = NULL;
-
-    if (*head == NULL)
-    {
-        *head = new_node;
-    }
-    else
-    {
-        list_t *tmp = *head;
-        while (tmp->next != NULL)
-        {
-            tmp = tmp->next;
-        }
-        tmp->next = new_node;
-    }
-
-    return (new_node);
+int length = _strlen(str);
+list_t *new_node = (list_t *)malloc(sizeof(list_t));
+if (str == NULL)
+{
+return (NULL);
+}
+if (new_node == NULL)
+{
+return (NULL);
+}
+new_node->str = strdup(str);
+if (new_node->str == NULL)
+{
+free(new_node);
+return (NULL);
+}
+new_node->len = length;
+new_node->next = NULL;
+if (*head == NULL)
+{
+*head = new_node;
+}
+else
+{
+list_t *tmp = *head;
+while (tmp->next != NULL)
+{
+tmp = tmp->next;
+}
+tmp->next = new_node;
+}
+return (new_node);
 }
