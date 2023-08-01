d28383ad4d151a2fb3242183122668a4924fc553
ayb1337 <aero.glowsky@gmail.com>
2023-08-01 21:52:31 +0100

    task 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - Deletes the head node of a listint_t linked list
+ * @head: Double pointer to the head of the linked list
+ * Return: The data (n) of the deleted head node, or 0 if the list is empty
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+unsigned int i = 0;
+
+for(; i < index; i++)
+{
+head = head->next;
+if (head == NULL)
+{
+return (NULL);
+}
+}
+return (head);
+}
