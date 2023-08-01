332c46dbe375396e19fc23ca99cd6c3dd55f1300
ayb1337 <aero.glowsky@gmail.com>
2023-08-01 12:12:29 +0100

    Third task

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+
+/**
+ * free_listint - prints all the elements of a list_t list
+ * @head: the list_t list to print all the elements of
+ * Return: returns the number of nodes
+ */
+
+void free_listint(listint_t *head);
+{
+istint_t *current;
+
+while (head != NULL)
+{
+current = head;
+head = head->next;
+free(current->str);
+free(current);
+}
+}
