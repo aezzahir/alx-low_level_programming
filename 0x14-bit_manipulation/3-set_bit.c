a467126e9b2e113c64e2df626efd1a907e5f3130
ayb1337 <aero.glowsky@gmail.com>
2023-08-03 20:29:06 +0100

    3.11 - set bit at index to 1

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,8 +11,15 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-unsigned long int mask = 1 << index;
-if (!(mask & *n))
+unsigned long int mask;
+
+if (index >= sizeof(unsigned long int) * 8)
+{
+return (-1);
+}
+
+mask = 1 << index;
+if ((mask & *n) == 0)
 {
 *n = *n | mask;
 return (1);
