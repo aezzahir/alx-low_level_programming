5c4468863b24c63eb73a30da487dbb4b9ab7c29a
ayb1337 <aero.glowsky@gmail.com>
2023-08-03 20:42:57 +0100

    4.100 set bit at index to 0

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,8 +12,13 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-unsigned long int mask = 1 << index;
+unsigned long int mask;
 
+if (index >= sizeof(unsigned long int) * 8)
+{
+return (-1);
+}
+mask = 1 << index;
 if ((n & mask) == mask)
 {
 return (1);
