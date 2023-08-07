433b5cf11094aa33e1bc1bb438342b761b2cb58c
ayb1337 <aero.glowsky@gmail.com>
2023-08-07 23:17:45 +0100

    gg

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,73 +2,72 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <errno.h>
 
 #define BUF_SIZE 1024
 
 /**
- * exit_error - Helper function to exit with an error message.
- * @exit_code: The exit code to use.
- * @message: The error message to print.
+ * error_exit - Display an error message and exit with the given status code.
+ * @msg: The error message to display.
+ * @exit_code: The exit status code.
  */
-void exit_error(int exit_code, const char *message)
+void error_exit(const char *msg, int exit_code)
 {
-dprintf(STDERR_FILENO, "%s\n", message);
+dprintf(STDERR_FILENO, "%s\n", msg);
 exit(exit_code);
 }
 
 /**
- * main - The main function.
- * @argc: The number of arguments.
- * @argv: An array of arguments.
+ * main - Entry point of the program.
+ * @argc: The argument count.
+ * @argv: The argument vector.
  *
- * Return: Always 0.
+ * Return: 0 on success, or the appropriate exit status code on failure.
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-int fd_from, fd_to, res;
-char buffer[BUF_SIZE];
+int fd_from, fd_to;
 ssize_t bytes_read, bytes_written;
+char buffer[BUF_SIZE];
 fd_from = open(argv[1], O_RDONLY);
-fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 
 if (argc != 3)
 {
-exit_error(97, "Usage: cp file_from file_to");
+error_exit("Usage: cp file_from file_to", 97);
 }
 
 if (fd_from == -1)
 {
-exit_error(98, "Error: Can't read from file");
+error_exit("Error: Can't read from file", 98);
 }
-
+fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 if (fd_to == -1)
 {
-exit_error(99, "Error: Can't write to file");
+close(fd_from);
+error_exit("Error: Can't write to file", 99);
 }
 while ((bytes_read = read(fd_from, buffer, BUF_SIZE)) > 0)
 {
 bytes_written = write(fd_to, buffer, bytes_read);
-if (bytes_written != bytes_read || bytes_written == -1)
+if (bytes_written != bytes_read)
 {
-exit_error(99, "Error: Can't write to file");
+close(fd_from);
+close(fd_to);
+error_exit("Error: Can't write to file", 99);
 }
 }
 if (bytes_read == -1)
 {
-exit_error(98, "Error: Can't read from file");
+close(fd_from);
+close(fd_to);
+error_exit("Error: Can't read from file", 98);
 }
-res = close(fd_from);
-if (res == -1)
+if (close(fd_from) == -1)
 {
-exit_error(100, "Error: Can't close fd");
+error_exit("Error: Can't close fd", 100);
 }
-res = close(fd_to);
-if (res == -1)
+if (close(fd_to) == -1)
 {
-exit_error(100, "Error: Can't close fd");
+error_exit("Error: Can't close fd", 100);
 }
 return (0);
 }
