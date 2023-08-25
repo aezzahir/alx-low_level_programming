0a35cf6fe53dec946d35cd11e48b6acae801c7f3
AYOUB <aero.glowsky@gmail.com>
2023-08-25 19:04:01 +0100

    100

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,4 +1,3 @@
-/* 100-elf_header.c */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -12,8 +11,10 @@
  */
 int main(int argc, char **argv)
 {
-	int fd;
+	int fd, i;
 	unsigned char e_ident[16];
+	unsigned short e_type;
+	unsigned long int e_entry;
 
 	if (argc != 2)
 	{
@@ -35,22 +36,37 @@ int main(int argc, char **argv)
 		exit(98);
 	}
 
-	if (e_ident[0] != 0x7f || e_ident[1] != 'E')
+	if (e_ident[0] != 0x7f || e_ident[1] != 'E' || e_ident[2] != 'L' || e_ident[3] != 'F')
 	{
-		if (e_ident[2] != 'L' || e_ident[3] != 'F')
-		{
-			fprintf(stderr, "%s is not an ELF file.\n", argv[1]);
-			close(fd);
-			exit(98);
-		}
+		fprintf(stderr, "%s is not an ELF file.\n", argv[1]);
+		close(fd);
+		exit(98);
 	}
+	lseek(fd, 16, SEEK_SET);
+	read(fd, &e_type, sizeof(e_type));
+	lseek(fd, 24, SEEK_SET);
+	read(fd, &e_entry, sizeof(e_entry));
 
 	printf("ELF Header:\n");
 	printf("  Magic:   ");
-	printf("%02x %02x", e_ident[0], e_ident[1]);
-	printf(" %02x %02x\n", e_ident[2], e_ident[3]);
-	/* Further fields can be added similarly */
+	for (i = 0; i < 16; i++)
+		printf("%02x ", e_ident[i]);
+	printf("\n");
+
+	printf("  Type:                              ");
+	switch (e_type)
+	{
+		case 2:
+			printf("EXEC (Executable file)\n");
+			break;
+		case 3:
+			printf("DYN (Shared object file)\n");
+			break;
+		default:
+			printf("<unknown: %x>\n", e_type);
+			break;
+	}
+	printf("  Entry point address:               %lx\n", e_entry);
 	close(fd);
 	return (0);
 }
-
