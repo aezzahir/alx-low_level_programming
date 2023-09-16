484609f50450865d9f3974a953afa0fa0619c8c9
Ayoub <aero.engineer1337@gmail.com>
2023-09-16 20:37:11 +0100

    Inset node at idx

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,6 +17,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (new_node == NULL)
 		return (NULL);
+	if (idx == 0)
+		return (add_dnodeint(h, n)); 
 	i = 0;
 	new_node->n = n;
 	temp = *h;
