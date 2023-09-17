df25ab9e6c34759454e7438e6bf2696b54264fde
Ayoub <aero.engineer1337@gmail.com>
2023-09-17 09:03:24 +0100

    delete node at index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -15,9 +15,15 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	temp = *head;
 	if (index == 0)
 	{
-		*head = (*head)->next;
-		free(temp);
-		return (1);
+		if (temp)
+		{
+			*head = temp->next;
+			if (*head)
+				(*head)->prev = NULL;
+			free(temp);
+			return (1);
+		}
+		return (-1);
 	}
 	i = 0;
 	while (i < index && temp != NULL)
@@ -25,7 +31,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		temp = temp->next;
 		i++;
 	}
-	if (i == index)
+	if (i == index && temp != NULL)
 	{
 		(temp->next)->prev = temp->prev;
 		(temp->prev)->next = temp->next;
